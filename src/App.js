9727d8de6832ad9b0d28f84be320c8a5a7c64552
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-08-03 14:59:13 +0200

    etape 2 done

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Weekly from "./pages/Weekly";
+import Popular from "./pages/Popular";
+import Favorites from "./pages/Favorites";
+import NotFound from "./pages/NotFound";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to={"/"}>Home</Link>
+              </li>
+              <li>
+                <Link to={"/weekly"}>Weekly</Link>
+              </li>
+              <li>
+                <Link to={"/popular"}>Popular</Link>
+              </li>
+              <li>
+                <Link to={"/favorites"}>Favorites</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/weekly" element={<Weekly />} />
+          <Route path="/popular" element={<Popular />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
