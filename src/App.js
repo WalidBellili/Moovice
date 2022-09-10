07fac3d04e0636d067c200e03e9956ed32621491
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-10 12:48:00 +0200

    set up of live sass done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
 import Popular from "./pages/Popular";
 import Weekly from "./pages/Weekly";
+import "./assets/styles/index.css";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="popular" element={<Popular />} />
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Moovice</h1>
+      <nav>
+        <ul>
+          <li>Popular</li>
+          <li>Weekly</li>
+          <li>Favorites</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
