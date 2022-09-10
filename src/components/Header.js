721e3b69f47c30ab81998d25c5cbe47204688682
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-10 14:22:23 +0200

    idea of two nav mobile/desktop

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,35 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
-    <header>
-      <h1>Moovice</h1>
+    <header className="header">
+      <div className="title">
+        <Link to="/">
+          <h1 className="h1">Moovice</h1>
+        </Link>
+      </div>
       <nav>
-        <ul>
+        <Link to="/popular">
           <li>Popular</li>
+        </Link>
+        <Link to="/weekly">
           <li>Weekly</li>
+        </Link>
+        <Link to="/favorites">
           <li>Favorites</li>
-        </ul>
+        </Link>
       </nav>
+      <article className="mobilNav">
+        <Link to="/popular">
+          <li>Popular</li>
+        </Link>
+        <Link to="/weekly">
+          <li>Weekly</li>
+        </Link>
+        <Link to="/favorites">
+          <li>Favorites</li>
+        </Link>
+      </article>
     </header>
   );
 };
