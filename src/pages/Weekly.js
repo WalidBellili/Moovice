24326e4083777254669917f20840971c072aeb05
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-11 12:20:44 +0200

    display weekly ok

--- src/pages/Weekly.js
+++ src/pages/Weekly.js
@@ -15,10 +15,33 @@ const Weekly = () => {
       `http://api.themoviedb.org/3/discover/movie?primary_release_date.gte=${LAST_WEEK}&primary_release_date.lte=${TODAY}&api_key=9f6028c7eca26f5709c3882ffd943beb`
     );
     const response = await request.json();
-    setWeeklyMovies(response);
+    setWeeklyMovies(response.results);
   };
-  console.log(weeklyMovies);
-  return <div></div>;
+  // console.log(weeklyMovies);
+
+  return (
+    <section>
+      <h1>Weekly Movies</h1>
+      <article>
+        {weeklyMovies.map((weekly) => {
+          return (
+            <article className="card-container-favorite">
+              <div className="card-favorite">
+                <img
+                  src={`https://image.tmdb.org/t/p/w300/${weekly.poster_path}`}
+                  alt={weekly.title}
+                />
+              </div>
+              <div className="description-favorite">
+                <p>{weekly.title}</p>
+                <p>{weekly.release_date}</p>
+              </div>
+            </article>
+          );
+        })}
+      </article>
+    </section>
+  );
 };
 
 export default Weekly;
