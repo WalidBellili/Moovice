4656fc3063c6e6b3568dbf15f4e56ed56d9d207b
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-29 17:06:49 +0200

    i handle the forgotten id in the fetch of favories

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
+import Card from "../components/Card";
+import Container from "../components/Container";
+import H1 from "../components/H1";
 
 const Favorites = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     fetchFavoriteMovies();
+    // eslint-disable-next-line
   }, []);
 
   const fetchFavoriteMovies = async () => {
@@ -21,15 +25,25 @@ const Favorites = () => {
     }
   };
 
-  const fetchMovieById = async () => {
+  const fetchMovieById = async (id) => {
     const request = await fetch(
-      `https://api.themoviedb.org/3/movie/{ID}?api_key=${process.env.REACT_APP_API_KEY}`
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}`
     );
     const response = await request.json();
     return response;
   };
+  console.log(movies);
 
-  return <></>;
+  return (
+    <Container>
+      <H1 text="Favorites" />
+      <section className="container">
+        {movies.map((movie) => {
+          return <Card movie={movie} />;
+        })}
+      </section>
+    </Container>
+  );
 };
 
 export default Favorites;
