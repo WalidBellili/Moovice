eaa6090c57b75f2bb7caca3ba307f8d4e76a6b6b
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-10-08 10:38:12 +0200

    page home ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,64 +1,77 @@
-
-import { useEffect, useState } from 'react'
-import MiniCard from '../components/MiniCard'
-import HomePreview from '../components/HomePreview'
-import '../App.css'
+import { useEffect, useState } from "react";
+import Card from "../components/Card";
+import HomePreview from "../components/HomePreview";
+import "../App.css";
 
 const Home = () => {
-  const [latestMovie, setLatestMovie] = useState(null)
-  const [topRatedMovies, setTopRatedMovies] = useState([])
-  const [nowPlayingMovies, setNowPlayingMovies] = useState([])
-  const [upcomingMovies, setUpcomingMovies] = useState([])
+  // const [latestMovie, setLatestMovie] = useState(null);
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
 
   useEffect(() => {
-    setMovies()
+    setMovies();
     // eslint-disable-next-line
-  }, [])
+  }, []);
 
-  const fetchMovies = async url => {
-    const request = await fetch(url)
-    const response = await request.json()
-    return response
-  }
+  const fetchMovies = async (url) => {
+    const request = await fetch(url);
+    const response = await request.json();
+    return response;
+  };
 
   const setMovies = async () => {
     const topRated = await fetchMovies(
       `https://api.themoviedb.org/3/movie/top_rated?api_key=fea42b1e836ac59816b8332e564dbb41`
-    )
-    setTopRatedMovies(topRated.results)
+    );
+    setTopRatedMovies(topRated.results);
+    console.log(topRatedMovies);
 
     const nowPlaying = await fetchMovies(
       `https://api.themoviedb.org/3/movie/now_playing?api_key=fea42b1e836ac59816b8332e564dbb41`
-    )
-    setNowPlayingMovies(nowPlaying.results)
+    );
+    setNowPlayingMovies(nowPlaying.results);
 
     const upcoming = await fetchMovies(
       `https://api.themoviedb.org/3/movie/upcoming?api_key=fea42b1e836ac59816b8332e564dbb41`
-    )
-    setUpcomingMovies(upcoming.results)
+    );
+    setUpcomingMovies(upcoming.results);
 
-    const latest = await fetchMovies(
-      `https://api.themoviedb.org/3/movie/latest?api_key=fea42b1e836ac59816b8332e564dbb41`
-    )
-    setLatestMovie(latest)
-  }
+    // const latest = await fetchMovies(
+    //   `https://api.themoviedb.org/3/movie/latest?api_key=fea42b1e836ac59816b8332e564dbb41`
+    // );
+    // setLatestMovie(latest);
+  };
 
   return (
     <>
       <h1>Home</h1>
-
-      {latestMovie && (
-        <section className='mt-5'>
-          <h2>Latest movie</h2>
-          <MiniCard movie={latestMovie} />
+      <main>
+        <h2>Top Rated</h2>
+        <section className="container">
+          {topRatedMovies.map((movie) => {
+            return <Card movie={movie} />;
+          })}
         </section>
-      )}
-
-      <HomePreview title='Top rated' movies={topRatedMovies} />
-      <HomePreview title='Now playing' movies={nowPlayingMovies} />
-      <HomePreview title='Upcoming' movies={upcomingMovies} />
+      </main>
+      <main>
+        <h2>nowPlayingMovies</h2>
+        <section className="container">
+          {nowPlayingMovies.map((movie) => {
+            return <Card movie={movie} />;
+          })}
+        </section>
+      </main>
+      <main>
+        <h2>upcomingMovies</h2>
+        <section className="container">
+          {upcomingMovies.map((movie) => {
+            return <Card movie={movie} />;
+          })}
+        </section>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
